a60fa045909609937fd71547328a8bce64c7f413
RobinKuntal01 <107622477+RobinKuntal01@users.noreply.github.com>
2024-03-02 20:59:31 +0000

    basic

---
+++ WeatherApp/engine.js
@@ -0,0 +1,6 @@
+let cityName = document.getElementById('cityName');
+let searchBtn = document.querySelector('.searchBtn');
+
+
+weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=e02aa4992954e71040e2748e80d4536f"
+
