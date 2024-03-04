e8a10bc8d8164f8350134dacd030ce50529d0c09
RobinKuntal01 <107622477+RobinKuntal01@users.noreply.github.com>
2024-03-04 18:49:44 +0000

    commit

--- WeatherApp/engine.js
+++ WeatherApp/engine.js
@@ -1,6 +1,46 @@
-let cityName = document.getElementById('cityName');
-let searchBtn = document.querySelector('.searchBtn');
+let button = document.querySelector('.myButton');
+let detailsHere = document.querySelector('.detailsHere')
+console.log(button);
+
+button.addEventListener('click' , function(e){
+    e.preventDefault();
+    let cityName = document.getElementById('cityName').value;
+    weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=e02aa4992954e71040e2748e80d4536f&units=metric`
+
+    fetch(weatherAPI)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Data:', data);
+        console.log(data.weather);
+     //   detailsHere.innerHTML = `${data.name} Current Temp: ${ Math.floor(data.main.temp)}`
+        document.querySelector('#city').innerHTML = data.name 
+        document.querySelector('#temp').innerHTML = Math.round(data.main.temp)+ " °C"
+        document.querySelector('#wind').innerHTML = data.wind.speed + " Km/hr"
+        document.querySelector('#humidity').innerHTML = data.main.humidity+ " %"
 
 
-weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=e02aa4992954e71040e2748e80d4536f"
+        // Do something with the data
+      })
+      .catch(error => {
+        console.error('There was a problem with your fetch operation:', error);
+        document.querySelector('#city').innerHTML = "Please a Correct City Name" 
+        document.querySelector('#temp').innerHTML = ""
+        document.querySelector('#wind').innerHTML = ""
+        document.querySelector('#humidity').innerHTML = ""
+      });
+    
+})
+
+
+
+
+
+
+
+
 
